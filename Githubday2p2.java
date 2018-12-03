73ea97ac10368bdb6ce75eadb7495ff4daad652c
ManasBommakanti <45392182+ManasBommakanti@users.noreply.github.com>
2018-12-02 19:44:59 -0500

    I did Part B of the project.
    I did the part where we have to turn the Array List into an integer at home.

--- Githubday2p2.java
+++ Githubday2p2.java
@@ -1,10 +1,30 @@
+import java.util.ArrayList;
+
 public class Githubday2p2{
 	public static void main(String[]args){
-		System.out.println("Hello");
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(8);
+   		list.add(2);
+    		list.add(1);
+    		list.add(3);
+
+    		System.out.println(createNum(list));
+		
+		/*System.out.println("Hello");
 		System.out.println("Hi Manas");
 		System.out.println("Hey Mithil");
 		System.out.println("Hey again Mithil");
 		System.out.println("Testing Errorrr");
+		*/
 
 	}
+	public static int createNum(ArrayList<Integer> list){
+    	int multiply = 1;
+    	int creation = 0;
+    	for(int i = list.size() - 1; i >= 0; i--){
+      		creation+=(list.get(i)*multiply);
+      		multiply*=10;
+    	}
+    	return creation;
 }
